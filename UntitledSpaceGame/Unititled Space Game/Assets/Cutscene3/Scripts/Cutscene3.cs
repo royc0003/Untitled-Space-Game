1df485193b38e803b0b4a1b037aaa4456c08d74e
Grace Mok <44459153+grace-mok@users.noreply.github.com>
2020-10-20 01:16:56 +0800

    added animation for cutscene3

--- UntitledSpaceGame/Unititled Space Game/Assets/Cutscene3/Scripts/Cutscene3.cs
+++ UntitledSpaceGame/Unititled Space Game/Assets/Cutscene3/Scripts/Cutscene3.cs
@@ -8,17 +8,26 @@ public class Cutscene3 : MonoBehaviour
 {
     public Button button;
     public GameObject UI;
+    public Animator animatorFuel;
     // Start is called before the first frame update
     void Start()
     {
+        animatorFuel.SetBool("pour",false);
         UI.SetActive(true);
         Cursor.visible = true;
         Cursor.lockState = CursorLockMode.None;
         button.onClick.AddListener(NextScene);
     }
     
-    public void NextScene()
-    {
+
+    public void NextScene() {
+        animatorFuel.SetBool("pour",true);
+        Invoke("LoadNextScene",3.0f);
+        return;
+        
+    }
+    public void LoadNextScene() {
         SceneManager.LoadScene("Cutscene4");
     }
+    
 }
