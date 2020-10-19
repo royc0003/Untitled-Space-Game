b86a0767a7f2ff790258f9e60d75d64d6becb6ba
Grace Mok <44459153+grace-mok@users.noreply.github.com>
2020-10-19 17:41:14 +0800

    Cutscene 3 base

---
+++ UntitledSpaceGame/Unititled Space Game/Assets/Cutscene3/Scripts/Cutscene3.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Cutscene3 : MonoBehaviour
+{
+    public Button button;
+    // Start is called before the first frame update
+    void Start()
+    {
+        button.onClick.AddListener(NextScene);
+    }
+
+    public void NextScene() {
+        Invoke("LoadNextScene",3.0f);
+        return;
+        
+    }
+    public void LoadNextScene() {
+        SceneManager.LoadScene("Cutscene2");
+    }
+    
+}
