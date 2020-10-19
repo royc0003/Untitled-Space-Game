4fc122fd7ab8211c09a8be3394291b10fab604d9
Grace Mok <44459153+grace-mok@users.noreply.github.com>
2020-10-20 01:27:18 +0800

    added animation for cutscene4

--- UntitledSpaceGame/Unititled Space Game/Assets/Cutscene4/Scripts/Cutscene4.cs
+++ UntitledSpaceGame/Unititled Space Game/Assets/Cutscene4/Scripts/Cutscene4.cs
@@ -7,14 +7,21 @@ using UnityEngine.SceneManagement;
 public class Cutscene4 : MonoBehaviour
 {
     public Button button;
+    public Animator animatorCamera;
     // Start is called before the first frame update
     void Start()
     {
+        animatorCamera.SetBool("camera_move",false);
         button.onClick.AddListener(NextScene);
     }
     
-    public void NextScene()
-    {
+    public void NextScene() {
+        animatorCamera.SetBool("camera_move",true);
+        Invoke("LoadNextScene",3.5f);
+        return;
+        
+    }
+    public void LoadNextScene() {
         SceneManager.LoadScene("Level4Start");
     }
 }
