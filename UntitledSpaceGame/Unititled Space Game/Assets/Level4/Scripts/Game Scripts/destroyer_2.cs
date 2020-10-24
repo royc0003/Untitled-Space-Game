ce6e2ab8dd1a01676b3b6b9c9d0f1a868f202025
Royce <royceang96@gmail.com>
2020-10-25 02:36:13 +0800

    Added Spawner

---
+++ UntitledSpaceGame/Unititled Space Game/Assets/Level4/Scripts/Game Scripts/destroyer_2.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destroyer_2 : MonoBehaviour
+{
+    public float lifeTime = 2f;
+    
+    // Start is called before the first frame update
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(lifeTime > 0){
+            lifeTime -= Time.deltaTime;
+            if(lifeTime <= 0){
+                Destruction();
+            }
+        }
+    }
+
+    void Destruction(){
+        Debug.Log("I am bullet destroyed" + this.gameObject);
+        spawner_2 someObject = this.gameObject.GetComponentInParent<spawner_2>();
+        Destroy(this.gameObject);
+        someObject.SpawnObject();
+    }
+}
---
+++ UntitledSpaceGame/Unititled Space Game/Assets/Level4/Scripts/Game Scripts/spawner_2.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawner_2 : MonoBehaviour
+{
+
+    public Transform spawnPos;
+    public GameObject spawnee;
+    public bool stopSpawning = false;
+    public float spawnTime;
+    public float spawnDelay;
+    // Start is called before the first frame update
+  
+  void Start() {
+      InvokeRepeating("SpawnObject", spawnTime, spawnDelay);
+      
+  }
+
+  public void SpawnObject(){
+        Instantiate(spawnee, spawnPos.position, spawnPos.rotation);
+
+        if(stopSpawning){
+            CancelInvoke("SpawnObject");
+        }
+  }
+
+    // Update is called once per frame
+    
+}
