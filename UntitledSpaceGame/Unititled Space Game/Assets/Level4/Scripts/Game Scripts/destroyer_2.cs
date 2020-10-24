82dd1e54237f25b32c710f2598a629aaf202dae7
Royce <royceang96@gmail.com>
2020-10-25 03:33:25 +0800

    fixed spawning script

--- UntitledSpaceGame/Unititled Space Game/Assets/Level4/Scripts/Game Scripts/destroyer_2.cs
+++ UntitledSpaceGame/Unititled Space Game/Assets/Level4/Scripts/Game Scripts/destroyer_2.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class destroyer_2 : MonoBehaviour
 {
-    public float lifeTime = 2f;
+    public float lifeTime = 50f;
     
     // Start is called before the first frame update
 
@@ -12,18 +12,21 @@ public class destroyer_2 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+       bool spawnOnce = GetComponentInParent<spawner_2>().isSpawnOnce();
         if(lifeTime > 0){
             lifeTime -= Time.deltaTime;
             if(lifeTime <= 0){
-                Destruction();
+                if(spawnOnce == true) return;
+                    Destruction();
+                
             }
         }
     }
 
     void Destruction(){
-        Debug.Log("I am bullet destroyed" + this.gameObject);
-        spawner_2 someObject = this.gameObject.GetComponentInParent<spawner_2>();
+        //Debug.Log("I am bullet destroyed" + this.gameObject);
+       //spawner_2 someObject = this.gameObject.GetComponentInParent<spawner_2>();
         Destroy(this.gameObject);
-        someObject.SpawnObject();
+        //someObject.SpawnObject();
     }
 }
--- UntitledSpaceGame/Unititled Space Game/Assets/Level4/Scripts/Game Scripts/spawner_2.cs
+++ UntitledSpaceGame/Unititled Space Game/Assets/Level4/Scripts/Game Scripts/spawner_2.cs
@@ -8,17 +8,39 @@ public class spawner_2 : MonoBehaviour
     public Transform spawnPos;
     public GameObject spawnee;
     public bool stopSpawning = false;
+    public bool spawnOnce = false;
+
+
     public float spawnTime;
     public float spawnDelay;
+    public AudioClip audioClip;
     // Start is called before the first frame update
   
+  
+  void Awake(){
+      if(spawnOnce == true){
+        CancelInvoke("SpawnObject");
+        Instantiate(spawnee, spawnPos.position, spawnPos.rotation);
+
+
+      }
+  }
   void Start() {
+      if(spawnOnce == true) return;
       InvokeRepeating("SpawnObject", spawnTime, spawnDelay);
+
+    
       
   }
 
   public void SpawnObject(){
         Instantiate(spawnee, spawnPos.position, spawnPos.rotation);
+        setSpawnTime();
+        setSpawnDelay();
+        if(audioClip != null){
+            AudioSource.PlayClipAtPoint(audioClip, transform.position, 50f);
+        }
+
 
         if(stopSpawning){
             CancelInvoke("SpawnObject");
@@ -27,4 +49,16 @@ public class spawner_2 : MonoBehaviour
 
     // Update is called once per frame
     
+    private void setSpawnTime(){
+        //lifeTime
+        this.spawnTime = Random.Range(50.0f, 100.0f);
+    }
+        private void setSpawnDelay(){
+            //How long before next spawn
+        this.spawnDelay = Random.Range(50.0f, 100.0f);
+    }
+
+    public bool isSpawnOnce(){
+        return this.spawnOnce;
+    }
 }
