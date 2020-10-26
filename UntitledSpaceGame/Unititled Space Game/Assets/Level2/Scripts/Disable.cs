794bfb5e127859ed9e57a156fd56a9d9077e4bbc
Sarah Yeoh <SYEOH003@e.ntu.edu.sg>
2020-10-26 16:01:06 +0800

    UI changes
    Changed text box alignment. Not sure how to skip camera panning

---
+++ UntitledSpaceGame/Unititled Space Game/Assets/Level2/Scripts/Disable.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Disable : MonoBehaviour
+{
+    
+    public void BtnOnClick(string text) { 
+        Text txt = transform.Find("Text").GetComponent<Text>();
+        txt.text = text;
+
+        //btn.SetActive(false);
+        
+    }
+
+    public void DisableCamera(){
+        Camera camera = gameObject.GetComponent<Camera>();
+        camera.enabled = false;
+    }
+
+    public void EnableCamera(){
+        Camera cam = gameObject.GetComponent<Camera>();
+        GetComponent<Camera>().enabled = true;
+    }
+}
