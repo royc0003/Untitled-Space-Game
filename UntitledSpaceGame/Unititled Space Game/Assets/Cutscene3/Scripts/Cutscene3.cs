db4eca2b1dddd168d5926e0f4fa496d208c30571
Grace Mok <44459153+grace-mok@users.noreply.github.com>
2020-11-05 14:34:43 +0800

    cr8 new cutscene 3

---
+++ UntitledSpaceGame/Unititled Space Game/Assets/Cutscene3/Scripts/Cutscene3.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Cutscene3 : MonoBehaviour
+{
+    public Button button;
+    public Animator animatorCamera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animatorCamera.SetBool("move",false);
+        button.onClick.AddListener(NextScene);
+    }
+
+    public void NextScene() {
+        button.gameObject.SetActive(false);
+        animatorCamera.SetBool("move",true);
+        Invoke("LoadNextScene",4.0f);
+        return;
+    }
+
+    public void LoadNextScene() {
+        SceneManager.LoadScene("Level1A");
+    }
+}
