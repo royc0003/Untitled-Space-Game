f35e14c5c36069f07f1fd3ace0192b221df6282f
Grace Mok <44459153+grace-mok@users.noreply.github.com>
2020-11-05 18:38:20 +0800

    ADDED MODELS

--- UntitledSpaceGame/Unititled Space Game/Assets/Cutscene3/Scripts/Cutscene3.cs
+++ UntitledSpaceGame/Unititled Space Game/Assets/Cutscene3/Scripts/Cutscene3.cs
@@ -6,16 +6,84 @@ using UnityEngine.SceneManagement;
 
 public class Cutscene3 : MonoBehaviour
 {
+    private Queue<string> sentences;
     public Button button;
+    public Text dialogueText;
     public Animator animatorCamera;
+    public Animator animatorText;
+    private int currentId;
+    public GameObject myFace;
+    public GameObject friendFace;
     // Start is called before the first frame update
     void Start()
     {
+        sentences = new Queue<string>();
+        button.onClick.AddListener(DisplayNextSentence);
+        currentId = 1;
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
         animatorCamera.SetBool("move",false);
-        button.onClick.AddListener(NextScene);
     }
 
-    public void NextScene() {
+    public void StartDialogue(Cutscene3Dialogue dialogue)
+    {
+        Invoke("ChangeValueOfOpenToTrue",12f);
+        
+        sentences.Clear();
+
+        foreach (string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence()
+    {
+        if (sentences.Count == 0)
+        {
+            animatorText.SetBool("IsOpen", false);
+            Invoke("EndDialogue", 3f);
+            return;
+        }
+
+        string sentence = sentences.Dequeue();
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence(sentence));
+        showFace();
+    }
+
+    IEnumerator TypeSentence(string sentence) {
+        dialogueText.text = "";
+        foreach (char letter in sentence.ToCharArray()){
+            dialogueText.text += letter;
+            yield return null;
+        }
+    }
+
+    public void showFace() {
+        switch (currentId) {
+            case 1:
+                myFace.SetActive(true);
+                friendFace.SetActive(false);
+                currentId++;
+                break;
+            case 2:
+                myFace.SetActive(false);
+                friendFace.SetActive(true);
+                currentId--;
+                break;
+        }
+    }
+
+    public void ChangeValueOfOpenToTrue()
+    {
+        animatorText.SetBool("IsOpen", true);
+    }
+
+    public void EndDialogue()
+    {
         button.gameObject.SetActive(false);
         animatorCamera.SetBool("move",true);
         Invoke("LoadNextScene",4.0f);
@@ -23,6 +91,6 @@ public class Cutscene3 : MonoBehaviour
     }
 
     public void LoadNextScene() {
-        SceneManager.LoadScene("Level1A");
+        SceneManager.LoadScene("Start");
     }
 }
